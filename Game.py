ee660b47dc86fbf69828ce6c918efd34bd05dbc5
Yousif Muhammed <97788470+muhammedyousif@users.noreply.github.com>
2024-02-26 09:54:18 +0100

    h

--- Game.py
+++ Game.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 import random
+
+from pygame import Vector2
+
 from Food import Food
 from Snake import Snake
 
@@ -12,20 +15,67 @@ class Game:
         screen = pygame.display.set_mode((cell_number*cell_size,cell_number*cell_size))
         clock =pygame.time.Clock()
         pygame.init()
-        food=Food(cell_number)
-        snake = Snake()
+        self.food=Food(cell_number)
+        self.snake = Snake()
+
+        SCREEN_UPDATE=pygame.USEREVENT
+        pygame.time.set_timer(SCREEN_UPDATE,150)
+
         while True:
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type==SCREEN_UPDATE:
+                    self.update()
+                if event.type==pygame.KEYDOWN:
+                    self.check_key(event)
             screen.fill(pygame.Color('dark green '))
-            snake.draw_snake(cell_size,screen)
-            food.draw_fruit(cell_size,screen)
+            self.draw_elements(screen)
             pygame.display.update()
             clock.tick(60)
+
+    def update(self):
+        self.snake.move_snake()
+        self.did_eat()
+        self.check_fail()
+
+    def draw_elements(self,screen):
+        self.snake.draw_snake(self.cell_size, screen)
+        self.food.draw_fruit(self.cell_size, screen)
 
     def surface(self,w,h)->pygame.surface:
         testsurface = pygame.Surface((w,h))
         return testsurface
+    def check_key(self,event):
+        if event.key==pygame.K_UP:
+            if self.snake.direction.y!=1:
+                self.snake.direction=Vector2(0,-1)
+        if event.key==pygame.K_DOWN:
+            if self.snake.direction.y!=-1:
+                self.snake.direction=Vector2(0,1)
+        if event.key==pygame.K_RIGHT:
+            if self.snake.direction.x!=-1:
+                self.snake.direction=Vector2(1,0)
+        if event.key==pygame.K_LEFT:
+            if self.snake.direction.x!=1:
+                self.snake.direction=Vector2(-1,0)
+    def did_eat(self):
+        if self.food.pos==self.snake.body[0]:
+            self.food_eaten()
+            self.snake.add_block()
+
+
+    def food_eaten(self):
+        self.food=Food(self.cell_number)
+
+    def check_fail(self):
+        if not 0<=self.snake.body[0].x <self.cell_number or not 0<=self.snake.body[0].y <self.cell_number:
+            self.game_over()
+        for block in self.snake.body[1:]:
+            if block==self.snake.body[ 0]:
+                self.game_over()
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
 
--- Snake.py
+++ Snake.py
@@ -2,8 +2,24 @@ import pygame
 from pygame.math import Vector2
 class Snake:
     def __init__(self):
-        self.body=[Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        self.body=[Vector2(5,10),Vector2(4,10),Vector2(3,10)]
+        self.direction=Vector2(1,0)
+        self.newblock=False
     def draw_snake(self,cell_size,screen):
         for block in self.body:
             block_rect=pygame.Rect(block.x*cell_size,block.y*cell_size,cell_size,cell_size)
             pygame.draw.rect(screen,(183,191,122),block_rect)
+    def move_snake(self):
+        if self.newblock==True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.newblock=False
+        else:
+            body_copy=self.body[:-1]
+            body_copy.insert(0,body_copy[0]+self.direction)
+            self.body=body_copy[: ]
+    def add_block(self):
+        self.newblock=True
+    def set_newblock(self,abool):
+        self.newblock=abool
